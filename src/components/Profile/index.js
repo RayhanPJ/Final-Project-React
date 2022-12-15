2abef62dd03438755179ac831afacb4ca98162f0
roro1900018020 <roro1900018020@webmail.uad.ac.id>
2022-12-16 00:05:54 +0700

    pop up

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "../Header";
 import Footer from "../Footer";
 
-
 const Profile = () => {
+    const [show, setShow] = useState(false);
+    
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const getApiDataProfile = async () => {
+        const response = await fetch(
+          "https://gotravel-production.up.railway.app/api/v1/profile"
+        ).then((response) => response.json());
+        
+        console.log(response.data);
+        
+      };
+      useEffect(() => {
+        getApiDataProfile();
+      }, []);
     return (
         <div>
         <Header />
@@ -21,8 +38,39 @@ const Profile = () => {
                     <h3 className="fw-bolder">Rayhan</h3>
                     <h5 className="text-muted">Jakarta</h5>
                     <p>Saya senang sekali Traveling keliling Indonesia.<br />Traveling ke tempat wisata seperti Candi.</p>
-                    <a href="#/" className="btn btn-lg btn-outline-dark">Edit Profile</a>
-                    <a href="#/" className="btn btn-lg btn-outline-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-share-fill" viewBox="0 0 16 16"><path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z"/></svg></a>
+                    
+                    <Button variant="btn btn-lg btn-outline-dark" onClick={handleShow}>
+                    Edit Foto Profile
+                    </Button>
+                    <a href="#" className="btn btn-lg btn-outline-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-share-fill" viewBox="0 0 16 16"><path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z"/></svg></a>
+                    
+                    <Modal
+                    show={show}
+                    onHide={handleClose}
+                    backdrop="static"
+                    keyboard={false}
+                    >
+                    <Modal.Header closeButton>
+                        <Modal.Title>Edit Foto</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                    <label htmlFor="colFormLabel" className="col-sm-2 col-form-label">Foto</label>
+                        <div className="col-sm-5">
+                          <input className="form-control" name="gambar" type="file" id="foto" placeholder="small"/>
+                          <small id="text" className="text-muted">
+                            File size max.2MB
+                          </small>
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                        Save
+                        </Button>
+                    </Modal.Footer>
+                    </Modal>
                 </div>
 
                 <div className="col-12 mt-5 text-center">
